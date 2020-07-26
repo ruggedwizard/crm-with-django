df001d48725b13d135a49283f7a78a5769265d3c
David Isaac Survive <40686166+ruggedwizard@users.noreply.github.com>
2020-07-26 19:02:35 +0100

    Add files via upload

---
+++ accounts/decorators.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwags):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return view_func(request, *args, **kwags)
+
+    return wrapper_func
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwags):
+            print('Working:',allowed_roles)
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+            if group in allowed_roles:
+
+                return view_func(request, *args, **kwags)
+            else:
+                return HttpResponse('You are not authorized to view this page')
+        return wrapper_func
+    return decorator
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwags):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name 
+        
+        if group == 'customer':
+            return redirect('user-page')
+        if group == 'admin':
+            return view_func(request, *args, **kwags)
+    return wrapper_func
